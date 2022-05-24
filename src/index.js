b09bf7c878223a99e37eb97c2384f06a5ad14114
Nick Ponomarev <ponomnick@yandex.ru>
2022-05-24 12:43:45 +1000

    add arguments and options

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import { Command, program } from "commander";
 
 const genDiff = () => {
-    const programm = new Command()
+    //const programm = new Command()
+//console.log('12')
+}
     program
     .description('Compares two configuration files and shows a difference.')
     .version('1.0.0')
-    program.parse();
-}
+    .action(genDiff)
+    .arguments('<filepath1> <filepath2>')
+    .option('-f, --format <type>', 'output format')
+    .parse(process.argv);
 export default genDiff;
