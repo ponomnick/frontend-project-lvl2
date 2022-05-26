32355b83da815347d184d3ed3cc34b5e3bb2a8e4
Nick Ponomarev <ponomnick@yandex.ru>
2022-05-26 17:07:04 +1000

    add __test__

---
+++ __tests__/index.test.js
@@ -0,0 +1,23 @@
+import { fileURLToPath } from 'url';
+// import { readFileSync } from 'node:fs';
+import fs from 'fs';
+import path, { dirname } from 'path';
+import genDiff, { getData, stringify } from '../src/index.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
+//console.log(__dirname);
+// console.log(readFile(file1));
+test('diff', () => {
+  
+    const file1 = readFile('file1.json').trim();
+    const file2 = readFile('file2.json').trim();
+    const diff = readFile('diff.json').trim();
+ 
+    const result = genDiff(file1, file2);
+    console.log(result)
+    expect(result).toEqual(diff);
+  });
