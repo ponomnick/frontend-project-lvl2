e93017c9cb67c3c2c727ada9085c141ad4c97aea
Nick Ponomarev <ponomnick@yandex.ru>
2022-05-29 14:32:30 +1000

    add test diff yaml

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -10,8 +10,14 @@ const __dirname = dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-test('diff', () => {
+test('diff json', () => {
   const diff = readFile('diff.json').trim();
   const result = genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'));
+  expect(result).toEqual(diff);
+});
+
+test('diff yaml', () => {
+  const diff = readFile('diff.yml').trim();
+  const result = genDiff(getFixturePath('file1.yml'), getFixturePath('file2.yml'));
   expect(result).toEqual(diff);
 });
