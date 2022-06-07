0cbb8aadbd47ecb2ba9e95ef435ebe1c6d296e1e
Nick Ponomarev <ponomnick@yandex.ru>
2022-06-07 16:20:48 +1000

    rewrite, delete return function

--- src/makeTree.js
+++ src/makeTree.js
@@ -2,28 +2,23 @@ import _ from 'lodash';
 
 const makeTree = (file1, file2) => {
   const uniqFile = { ...file1, ...file2 };
-
   const keys = Object.keys(uniqFile);
+  return _.sortBy(keys).map((key) => {
+    if (typeof uniqFile[key] === 'object' && uniqFile[key] !== null && _.has(file1, key) && _.has(file2, key)) {
+      const children = makeTree(file1[key], file2[key]);
+      return { key, children, status: 'parent' };
+    }
 
-  const sortKeys = _.sortBy(keys)
-    .map((key) => {
-      if (typeof uniqFile[key] === 'object' && uniqFile[key] !== null && _.has(file1, key) && _.has(file2, key)) {
-        const children = makeTree(file1[key], file2[key]);
-        return { key, children, status: 'parent' };
-      }
-
-      if (!_.has(file1, key)) {
-        return { key, status: 'added', value: file2[key] };
-      } if (!_.has(file2, key)) {
-        return { key, status: 'delete', value: file1[key] };
-      } if (file2[key] !== file1[key]) {
-        return {
-          key, status: 'changed', value1: file1[key], value2: file2[key],
-        };
-      }
-      return { key, status: 'unchanged', value: file1[key] };
-    });
-
-  return sortKeys;
+    if (!_.has(file1, key)) {
+      return { key, status: 'added', value: file2[key] };
+    } if (!_.has(file2, key)) {
+      return { key, status: 'delete', value: file1[key] };
+    } if (file2[key] !== file1[key]) {
+      return {
+        key, status: 'changed', value1: file1[key], value2: file2[key],
+      };
+    }
+    return { key, status: 'unchanged', value: file1[key] };
+  });
 };
 export default makeTree;
