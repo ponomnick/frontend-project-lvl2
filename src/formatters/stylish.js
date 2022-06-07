f5a6df0dc2157fe671a97ad45b1444c39a07b625
Nick Ponomarev <ponomnick@yandex.ru>
2022-06-07 16:04:42 +1000

    add folder formatters

---
+++ src/formatters/stylish.js
@@ -0,0 +1,50 @@
+const indent = (depth, spaceCount = 4) => ' '.repeat(spaceCount * depth - 2);
+
+const stringify = (value, depth) => {
+  if (typeof value !== 'object' || value === null) {
+    return `${value}`;
+  }
+  const lines = Object
+    .entries(value)
+    .map(([key, val]) => `${indent(depth + 1)}  ${key}: ${stringify(val, depth + 1)}`);
+  return [
+    '{',
+    ...lines,
+    `${indent(depth)}  }`,
+  ].join('\n');
+};
+
+const stylish = (tree) => {
+  const iter = (data, depth) => {
+    const result = data
+      .map((node) => {
+        const getValue = (value, replacer) => `${indent(depth)}${replacer} ${node.key}: ${stringify(value, depth)}\n`;
+
+        const { status } = node;
+        switch (status) {
+          case 'added': {
+            return getValue(node.value, '+');
+          }
+          case 'delete': {
+            return getValue(node.value, '-');
+          }
+          case 'changed': {
+            return `${getValue(node.value1, '-')}${getValue(node.value2, '+')}`;
+          }
+          case 'unchanged': {
+            return getValue(node.value, ' ');
+          }
+          case 'parent': {
+            return `${indent(depth)}  ${node.key}: {\n${iter(node.children, depth + 1).join('')}${indent(depth)}  }\n`;
+          }
+          default: {
+            throw new Error(`Несуществующий статус: ${status}`);
+          }
+        }
+      });
+    return result;
+  };
+  return `{\n${iter(tree, 1).join('')}}`;
+};
+
+export default stylish;
