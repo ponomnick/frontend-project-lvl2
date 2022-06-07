447cc34584f431f7d381c8bbec21550342ae720b
Nick Ponomarev <ponomnick@yandex.ru>
2022-06-07 16:28:12 +1000

    rewrite, add isPlainObject

--- src/makeTree.js
+++ src/makeTree.js
@@ -4,11 +4,10 @@ const makeTree = (file1, file2) => {
   const uniqFile = { ...file1, ...file2 };
   const keys = Object.keys(uniqFile);
   return _.sortBy(keys).map((key) => {
-    if (typeof uniqFile[key] === 'object' && uniqFile[key] !== null && _.has(file1, key) && _.has(file2, key)) {
+    if (_.isPlainObject(file1[key]) && _.isPlainObject(file2[key])) {
       const children = makeTree(file1[key], file2[key]);
       return { key, children, status: 'parent' };
     }
-
     if (!_.has(file1, key)) {
       return { key, status: 'added', value: file2[key] };
     } if (!_.has(file2, key)) {
