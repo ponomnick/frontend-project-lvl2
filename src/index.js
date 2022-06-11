4cba3b4603ad47fe0509a75e0d0e8850d2241e8e
Nick Ponomarev <ponomnick@yandex.ru>
2022-06-11 16:09:56 +1000

    step 7

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import fs from 'fs';
 import { cwd } from 'process';
 import path from 'path';
 import getDataParse from './parsers.js';
-import stylish from './formatters/stylish.js';
+import format from './formatters/index.js';
 import makeTree from './makeTree.js';
 
 const getData = (pathToFile) => {
@@ -13,7 +13,7 @@ const getData = (pathToFile) => {
   return contentFile;
 };
 
-const genDiff = (filepath1, filepath2) => {
+const genDiff = (filepath1, filepath2, formatName = 'stylish') => {
   const fileExt1 = path.extname(filepath1);
   const fileExt2 = path.extname(filepath2);
   const file1 = getDataParse(getData(filepath1), fileExt1);
@@ -21,6 +21,6 @@ const genDiff = (filepath1, filepath2) => {
 
   const innerView = makeTree(file1, file2);
 
-  return stylish(innerView);
+  return format(innerView, formatName);
 };
 export default genDiff;
