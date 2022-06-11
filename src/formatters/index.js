9868752216dd2d4ba6446dd02a31c2a2829c14fa
Nick Ponomarev <ponomnick@yandex.ru>
2022-06-11 16:07:39 +1000

    add formatter.index.js

---
+++ src/formatters/index.js
@@ -0,0 +1,15 @@
+import plain from './plain.js';
+import stylish from './stylish.js';
+
+const format = (tree, formatName) => {
+  switch (formatName) {
+    case 'plain':
+      return plain(tree);
+    case 'stylish':
+      return stylish(tree);
+
+    default:
+      throw new Error(`Неверный формат: ${formatName}`);
+  }
+};
+export default format;
